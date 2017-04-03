8afc6a9e3d7dd91154c66ad0aa1a32ea659aab35
Mark Alston <malston@pivotal.io>
2017-04-03 07:15:46 -0600

    Configure sample to use main module config properties

--- sample/src/main/java/io/pivotal/config/client/sample/ConfigClientConfiguration.java
+++ sample/src/main/java/io/pivotal/config/client/sample/ConfigClientConfiguration.java
@@ -1,13 +1,14 @@
 package io.pivotal.config.client.sample;
 
 import io.pivotal.config.client.ConfigClientTemplate;
+import io.pivotal.config.client.ConfigClientTemplateProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(ConfigClientTemplateProperties.class)
-public class ClientConfiguration {
+public class ConfigClientConfiguration {
 
     @Bean
     public ConfigClientTemplate configClientTemplate(ConfigClientTemplateProperties properties) {

a2d7019544953d3686e371961cd5c32bfe8a164f
Mark Alston <malston@pivotal.io>
2017-04-08 01:00:04 -0600

    Testing

--- ci/tasks/generate-settings.sh
+++ ci/tasks/generate-settings.sh
@@ -4,6 +4,27 @@ mkdir -p ${HOME}/.m2
 mkdir -p ${HOME}/.gradle
 
 ROOT_IN_M2_RESOURCE="${ROOT_FOLDER}/${M2_REPO}/root"
+# export M2_HOME="${ROOT_IN_M2_RESOURCE}/.m2"
+# export NEW_LOCAL_REPO="${M2_HOME}/repository/"
+
+cat > ${HOME}/.m2/settings.xml <<EOF
+
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
+      <servers>
+        <server>
+          <id>${M2_SETTINGS_REPO_ID}</id>
+          <username>${M2_SETTINGS_REPO_USERNAME}</username>
+          <password>${M2_SETTINGS_REPO_PASSWORD}</password>
+        </server>
+      </servers>
+</settings>
+
+EOF
+echo "Settings xml written"
+
 export GRADLE_USER_HOME="${ROOT_IN_M2_RESOURCE}/.gradle"
 
 echo "Writing gradle.properties to [${GRADLE_USER_HOME}/gradle.properties]"
@@ -17,4 +38,5 @@ EOF
 echo "gradle.properties written"
 
 echo "Moving [${GRADLE_USER_HOME}] [${HOME}] folder"
-mv ${GRADLE_USER_HOME} ${HOME}/.gradle
+# mv ${NEW_LOCAL_REPO} ${HOME}/.m2/repository
+mv ${GRADLE_USER_HOME}/wrapper ${HOME}/.gradle/wrapper

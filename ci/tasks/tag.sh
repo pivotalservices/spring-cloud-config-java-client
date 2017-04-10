13ba33263f2dd62a96e3a5e1ba942a982f62a7ff
Mark Alston <malston@pivotal.io>
2017-04-10 13:46:35 -0600

    Fix init tag prefix

--- ci/tasks/tag.sh
+++ ci/tasks/tag.sh
@@ -4,7 +4,7 @@ export ROOT_FOLDER=$( pwd )
 export REPO_RESOURCE=repo
 export VERSION_RESOURCE=version
 export OUTPUT_RESOURCE=out
-export TAG_PREFIX=${TAG_PREFIX:~prod}
+export TAG_PREFIX="${TAG_PREFIX:-prod}"
 
 echo "Root folder is [${ROOT_FOLDER}]"
 echo "Repo resource folder is [${REPO_RESOURCE}]"
@@ -15,6 +15,6 @@ source ${ROOT_FOLDER}/${REPO_RESOURCE}/ci/tasks/pipeline.sh
 echo "Building and uploading the projects artifacts"
 cd ${ROOT_FOLDER}/${REPO_RESOURCE}
 
-echo "Tagging the project with ${TAG_PREFIX} tag"
+echo "Tagging the project with [${TAG_PREFIX}] tag"
 echo "${TAG_PREFIX}/${PIPELINE_VERSION}" > ${ROOT_FOLDER}/${REPO_RESOURCE}/tag
 cp -r ${ROOT_FOLDER}/${REPO_RESOURCE}/. ${ROOT_FOLDER}/${OUTPUT_RESOURCE}/

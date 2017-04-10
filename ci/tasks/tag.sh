08c50028292cfd4eb47c5978ae450a42f9066c6a
Mark Alston <malston@pivotal.io>
2017-04-10 11:22:55 -0600

    Extrapolate tagging into own task

--- ci/tasks/tag.sh
+++ ci/tasks/tag.sh
@@ -4,6 +4,7 @@ export ROOT_FOLDER=$( pwd )
 export REPO_RESOURCE=repo
 export VERSION_RESOURCE=version
 export OUTPUT_RESOURCE=out
+export TAG_PREFIX=${TAG_PREFIX:~prod}
 
 echo "Root folder is [${ROOT_FOLDER}]"
 echo "Repo resource folder is [${REPO_RESOURCE}]"
@@ -14,4 +15,6 @@ source ${ROOT_FOLDER}/${REPO_RESOURCE}/ci/tasks/pipeline.sh
 echo "Building and uploading the projects artifacts"
 cd ${ROOT_FOLDER}/${REPO_RESOURCE}
 
-. ${SCRIPTS_OUTPUT_FOLDER}/build_and_upload.sh
+echo "Tagging the project with ${TAG_PREFIX} tag"
+echo "${TAG_PREFIX}/${PIPELINE_VERSION}" > ${ROOT_FOLDER}/${REPO_RESOURCE}/tag
+cp -r ${ROOT_FOLDER}/${REPO_RESOURCE}/. ${ROOT_FOLDER}/${OUTPUT_RESOURCE}/
